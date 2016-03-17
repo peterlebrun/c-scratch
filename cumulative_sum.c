f62ec7cf8bae40bc8e1026c18b5a19ada3b5982d
peter <peter@aws>
2016-03-16 22:36:17 -0400

    Updating comment

--- cumulative_sum.c
+++ cumulative_sum.c
@@ -2,6 +2,9 @@
 #include <inttypes.h>
 
 int main(int argc, char *argv[]) {
+  // Notice what we have to do here to get the integer value
+  // from the command line arguments
+  // This was tricky to wrap my head around
   char* endptr;
   int x = (int) strtoimax(argv[1], &endptr, 10);
   int cum_sum = ((x + 1) * x) / 2;

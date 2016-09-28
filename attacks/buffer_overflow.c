b3230d60fe3af30defecfbbf794e4810e05fc8eb
Peter LeBrun <peterlebrun@gmail.com>
2016-09-27 22:35:28 -0400

    Add in some tips on compiling a buffer overflow with a mac

--- attacks/buffer_overflow.c
+++ attacks/buffer_overflow.c
@@ -4,6 +4,19 @@
  * This was written solely for the academic purpose of understanding
  * attacks in an effort to be a better defensive programmer.
  *
+ * This will probably compile fine with gnu gcc, but if you're having
+ * problems where it either won't compile, or you're getting abort traps,
+ * you'll need to compile like so:
+ *
+ * gcc buffer_overflow.c -fno-stack-protector -D_FORTIFY_SOURCE=0 -o buffer_overflow.c
+ *
+ * -fno-stack-protector tells gcc to compile even though you're using a dangerous
+ * command (strcpy as opposed to strncopy)
+ *
+ * -D_FORTIFY_SOURCE=0 tells gcc not to throw the abort trap
+ *
+ * see http://stackoverflow.com/questions/3026361/performing-simple-buffer-overflow-on-mac-os-10-6 for more details
+ *
  * Use at your own risk.
  */
 
@@ -21,7 +34,7 @@ int main(int argc, char **argv) {
   // we aren't validating anything, checking boundaries, etc
   strcpy(buf, "a1");
 
-  if (1 == foo) { // we never assigned any value to foo except 0
+  if (foo) { // we never assigned any value to foo except 0
     // Imagine if this was a super sensitive critical path
     // like maybe foo == 1 would allow advanced system permissions
     // or delete all your cat pictures
